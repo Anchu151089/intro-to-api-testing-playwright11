94a0f78203067bd2423c62561b90c33512a4597a
anchu <anchualavungal@gmail.com>
2025-06-28 15:21:08 +0300

    lesson 11 after some ccode changes

--- tests/api-auth/api-auth.spec.ts
+++ tests/api-auth/api-auth.spec.ts
@@ -10,10 +10,10 @@ test('login for students with incorrect username and password receive 401', asyn
     data: loginData,
   })
   expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
-
 })
 test('login for students with correct username and password receive 401', async ({ request }) => {
-  const loginData = LoginDto.createLoginWithCorrectData()
+  // const loginData = LoginDto.createLoginWithCorrectData()
+  const loginData = new LoginDto('testautomvl', 'whs4s5qbYbfT2n')
   const response = await request.post(authUrl, {
     data: loginData,
   })
