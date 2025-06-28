423a36eaae243e9a4fc6a92aa69afaf1b1056dc9
anchu <anchualavungal@gmail.com>
2025-06-28 14:24:17 +0300

    lesson 11 login with valid and invalid credentials with some modification

--- tests/api-auth/api-auth.spec.ts
+++ tests/api-auth/api-auth.spec.ts
@@ -5,11 +5,14 @@ import { StatusCodes } from 'http-status-codes'
 const authUrl ='https://backend.tallinn-learning.ee/login/student'
 
 test('login for students with incorrect username and password receive 401', async ({ request }) => {
+
   const loginData = new LoginDto('string11','string11')
   const response = await request.post(authUrl, {
     data: loginData,
   })
+
   expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
+
 })
 test('login for students with correct username and password receive 401', async ({ request }) => {
 
@@ -17,6 +20,7 @@ test('login for students with correct username and password receive 401', async 
   const response = await request.post(authUrl, {
     data: loginData,
   })
+
   const responseBody = await response.text()
   console.log('Response body :' + responseBody)
   expect(response.status()).toBe(StatusCodes.OK)
